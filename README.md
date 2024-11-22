64cf8223145ea86582bd80e2994c8bfb3a67aa1e
Pedro <112666350+pedroantonioc@users.noreply.github.com>
2024-11-21 22:53:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# A3-2024-2-React
+ App de lista de tarefas com react e vite
---
+++ src/components/Todoinput.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export default function TodoInput(props) {
+    const { handleAddTodos, todoValue, setTodoValue } = props
+    
+
+    //Pega a frase do input, desce como um card ao clicar no botão, e retorna ao valor incial "digite a tarefa"
+    return (
+        <header>
+            <input value={todoValue} onChange={ (e) => { 
+                setTodoValue(e.target.value)
+                }} placeholder="Digite a tarefa..." />
+            <button onClick={() => {
+                handleAddTodos(todoValue)
+                setTodoValue('')
+            }}>Add</button>
+        </header>
+    )
+}
